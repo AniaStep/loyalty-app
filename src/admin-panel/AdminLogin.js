18bd3b31b3eca96fe2a162cbfa34a7dbc7fe4ef1
Anna Stępień <147930506+AniaStep@users.noreply.github.com>
2024-05-14 20:40:01 +0200

    Customized client login and reset form

--- src/admin-panel/AdminLogin.js
+++ src/admin-panel/AdminLogin.js
@@ -3,13 +3,17 @@ import { useNavigate } from "react-router-dom";
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth, db } from '../firebase/config';
 import { PasswordResetForm } from "../misc/PasswordResetForm";
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import TextField from "@mui/material/TextField";
+
 
 export const AdminLogin = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [adminId, setAdminId] = useState(null);
     const [error, setError] = useState(null);
-
     const navigate = useNavigate();
 
     const errorsPL = {
@@ -19,9 +23,18 @@ export const AdminLogin = () => {
 
         const signIn = async () => {
         try {
+            const adminsCollectionRef = collection(db, 'admins');
+            const adminQuery = query(adminsCollectionRef, where('email', '==', email));
+            const adminSnapshot = await getDocs(adminQuery);
+
+            if (adminSnapshot.empty) {
+                setError('Podany użytkownik nie istnieje w bazie danych.');
+                return;
+            }
+
             const userCredential = await signInWithEmailAndPassword(auth, email, password);
             setAdminId(userCredential.user.uid);
-            navigate(`/admin/${userCredential.user.uid}`);
+            navigate(`/admin/${userCredential.user.uid}/dashboard`);
         } catch (err) {
             setError(errorsPL[err.code] || 'Wystąpił błąd podczas logowania.');
             console.error(err);
@@ -33,14 +46,29 @@ export const AdminLogin = () => {
         return <PasswordResetForm/>
     }
 
+    const handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+            signIn();
+        }
+    };
+
     return (
-        <div style={{display: "flex", flexDirection: "column", width: 300, gap: 10}}>
-            <h1> Rejestracja / Logowanie Admina</h1>
-            <input placeholder="email" onChange={(e) => setEmail(e.target.value)}/>
-            <input placeholder="hasło" type="password" onChange={(e) => setPassword(e.target.value)}/>
-            <button onClick={signIn}>Zaloguj się</button>
+        <Paper className="login-form">
+            <h1> Logowanie</h1>
+            <TextField className="login-input"
+                placeholder="email"
+                onChange={(e) => setEmail(e.target.value)}
+                onKeyDown={handleKeyPress}
+            />
+            <TextField className="login-input"
+                placeholder="hasło"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                onKeyDown={handleKeyPress}
+            />
+            <Button variant="contained" onClick={signIn}>Zaloguj się</Button>
             <p onClick={passwordReset} style={{ cursor: "pointer", color: "blue" }}>Nie pamiętam hasła</p>
             {error && <p>{error}</p>}
-        </div>
+        </Paper>
     )
 }
--- src/misc/PasswordResetInfo.js
+++ src/misc/PasswordResetInfo.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useLocation } from "react-router-dom";
-import "../App.css";
+import {collection, getDocs, query, where} from "firebase/firestore";
+import {db} from "../firebase/config";
 
 export const PasswordResetInfo = () => {
 
     const location = useLocation();
     const isClient = location.pathname.split("/")[1];
+    const adminId = location.pathname.split("/")[2];
+    const [adminsLogoURL, setAdminsLogoURL] = useState("");
+
+    useEffect(() => {
+        const fetchAdminData = async () => {
+            try {
+                const adminsQuery = query(collection(db, 'admins'), where('adminId', '==', adminId));
+                const adminsSnapshot = await getDocs(adminsQuery);
+                const adminsData = adminsSnapshot.docs.map(doc => doc.data())[0];
+
+                if (adminsData) {
+                    setAdminsLogoURL(adminsData.logoURL);
+                }
+
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchAdminData();
+    }, [adminId]);
+
     return(
-        <div className={isClient === "client" ? "Client-style" : "App"}>
+        <>
+            <div>{isClient === "client" ? <img src={adminsLogoURL}/> : "LoyalApp"}</div>
             <h1>Sprawdź email</h1>
-        </div>
+        </>
     )
 }
